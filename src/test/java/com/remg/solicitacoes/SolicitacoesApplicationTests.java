8753af536b0531c9282d1de70a764069fc3ecba6
Davi de Assis Gomes de Almeida <davi.almeida@oletecnologia.com.br>
2023-01-23 18:43:17 -0300

    init project

---
+++ src/test/java/com/remg/solicitacoes/SolicitacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remg.solicitacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
